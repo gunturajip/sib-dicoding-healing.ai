a5deee0d42de5810f70f82585442514cc1a5f26d
gunturajip <gunturaji852@gmail.com>
2022-12-05 16:09:19 +0700

    fixing cors again and Alhamdulillah

--- index.py
+++ index.py
@@ -1,29 +1,84 @@
 # import library
-from flask import Flask, request
+from flask import Flask, make_response, request, current_app
+from datetime import timedelta
+from functools import update_wrapper
 import pandas as pd
-# from flask_cors import CORS
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-
 
 # inisiasi object flask
 app = Flask(__name__)
 taman = pd.read_csv("taman.csv")
 museum = pd.read_csv("museum.csv")
 
-# inisiasi object flask_cors
-# CORS(app)
+def crossdomain(
+    origin=None,
+    methods=None,
+    headers=None,
+    expose_headers=None,
+    max_age=21600,
+    attach_to_all=True,
+    automatic_options=True,
+    credentials=False,
+):
+    """
+    http://flask.pocoo.org/snippets/56/
+    """
+    if methods is not None:
+        methods = ", ".join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, str):
+        headers = ", ".join(x.upper() for x in headers)
+    if expose_headers is not None and not isinstance(expose_headers, str):
+        expose_headers = ", ".join(x.upper() for x in expose_headers)
+    if not isinstance(origin, str):
+        origin = ", ".join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers["allow"]
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == "OPTIONS":
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != "OPTIONS":
+                return resp
+
+            h = resp.headers
+
+            h["Access-Control-Allow-Origin"] = origin
+            h["Access-Control-Allow-Methods"] = get_methods()
+            h["Access-Control-Max-Age"] = str(max_age)
+            if credentials:
+                h["Access-Control-Allow-Credentials"] = "true"
+            if headers is not None:
+                h["Access-Control-Allow-Headers"] = headers
+            if expose_headers is not None:
+                h["Access-Control-Expose-Headers"] = expose_headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+
+    return decorator
 
 # inisiasi variabel kosong bertipe dictionary (= json)
 identitas = {}
 
-
-@app.route("/api", methods=["GET"])
+@app.route('/api', methods=['GET','OPTIONS'])
+@crossdomain(origin="*")
 def get():
     return identitas
 
-
-@app.route("/api", methods=["POST"])
+@app.route('/api', methods=['POST','OPTIONS'])
+@crossdomain(origin="*")
 def post():
     global identitas
     identitas = {}
